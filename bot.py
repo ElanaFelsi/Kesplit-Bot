24ee8e0e2e40383f84716b4e71286839d8174188
hadasswittow <hadasswittow@gmail.com>
2019-11-12 12:53:01 +0200

    adding bot to group settings

--- bot.py
+++ bot.py
@@ -1,4 +1,63 @@
-import secret_settings
-print(secret_settings.BOT_TOKEN)
+from settings import *
 
-# YOUR BOT HERE
+
+
+
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
+
+
+
+
+
+
+def respond(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    text = str(update.message.text)
+    if text.startswith("$"):
+        print("good message")
+
+
+def get_help(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    f_name= update.message.from_user.first_name
+    context.bot.send_message(chat_id=chat_id, text=f"Don't worry' Im here for the rescue 💪💪! ")
+
+    logger.info(f"! {f_name} asked for help!")
+def added_to_group(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    group_name = update.effective_chat.title
+    context.bot.send_message(chat_id=chat_id, text=f"Welcome all 😄, I am your money manager! "
+                                                   f"If you want to join splitting money💰, just say $. "
+                                                   f"When you need my help hit /help.")
+
+
+
+
+
+    #create_db(chat_id,group_name)
+    #add a new data base for the group information
+
+#start_handler = CommandHandler('start', start)
+help_handler=CommandHandler('help',get_help)
+#dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+
+updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, added_to_group))
+
+echo_handler = MessageHandler(Filters.text, respond)
+dispatcher.add_handler(echo_handler)
+logger.info("* Start polling...")
+updater.start_polling()  # Starts polling in a background thread.
+updater.idle()  # Wait until Ctrl+C is pressed
+logger.info("* Bye!")
+
--- settings.py
+++ settings.py
@@ -1,2 +1,20 @@
 # YOUR NOT-SECRET SETTINGS HERE
 # DON'T PUT YOUR SECRET TOKEN HERE!!!!
+
+import secret_settings
+import logging
+import telegram
+import telepot as telepot
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
+    Filters, Updater
+
+
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=secret_settings.BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
